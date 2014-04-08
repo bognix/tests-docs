42c6955ad10e5ad97b8c4d77a7f273f4f6e7a25c
Bogna Knychala <bogna.ka@gmail.com>
2014-04-08 23:42:24 +0200

    Remove obsolete import

--- src/ClassesAndMethodsToXML.java
+++ src/ClassesAndMethodsToXML.java
@@ -8,7 +8,6 @@ import com.sun.tools.doclets.standard.Standard;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
